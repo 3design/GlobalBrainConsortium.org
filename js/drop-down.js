bfeeb7dd22442bcd897299048133595b02eed882
D <derek.lo@mcgill.ca>
2020-01-23 14:13:28 -0500

    initial push of new code
    This is the meant as a replacement site for the gbc2020

---
+++ js/drop-down.js
@@ -0,0 +1,15 @@
+(function($) { // Begin jQuery
+  $(function() { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
